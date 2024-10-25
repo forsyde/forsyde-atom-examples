9ae6457ffc362505a4804b0ab0d20855cafa49de
Ingo Sander <ingo@kth.se>
2024-10-25 09:21:26 +0200

    Initial executable SY_RTR_Model

--- reconfig/src/SY_RTR_Model.hs
+++ reconfig/src/SY_RTR_Model.hs
@@ -7,11 +7,29 @@ import ForSyDe.Atom.MoC.SY
 import ForSyDe.Atom.MoC (takeS)
 import ForSyDe.Atom.ExB.Absent
 
+-- |The 'rtrSystem' models a run-time reconfigurable system, where the active function is selected via a trigger signal. The trigger signal specifies the index of the function that shall be performed.
+rtrSystem :: Num a => [a -> a]
+                   -> Signal (Maybe Integer) -- ^The trigger signal specifies the index of the function to be activated
+                   -> Signal a           -- ^The input signal
+                   -> Signal a           -- ^The output signal
+rtrSystem configurations s_trigger s_in = s_out
+   where
+     s_out = worker s_conf s_in
+     s_initiate = handler s_trigger
+     (s_fetch, s_conf) = steward s_initiate s_data
+     s_data = delay (+1) (configRepo configurations s_fetch)
+-- >>>  rtrSystem [(+1), (+2), (*10)] (signal [Nothing, Just 1, Just 1, Just 2, Just 3]) (signal [1..20])
+-- {2,3,5,6,50}
+
 {- |
 The 'handler' initiates the reconfigurations by telling the Steward to fetch a given
 configuration from the configuration repository.
 -}
-handler = undefined
+handler :: Signal (Maybe Integer) -- ^The trigger signal specifies the index of the function to be activated
+        -> Signal (Maybe Integer) -- ^The initiation signal specifies the index of the function to be loaded
+handler = comb11 h
+  where h Nothing  = Nothing
+        h (Just x) = Just x
 
 -- | The 'worker' executes the functionality provided by the 'steward'
 --   with parameters from the 'handler'.  
@@ -26,9 +44,23 @@ worker = reconfig11
 
 -- | The 'steward' starts the reconfiguration process and “informs” the 'worker'
 --   that it is under reconfiguration and then fully configured. This could also be
---   achievd by mode configuration. 
-steward = undefined
-
+--   achievd by mode configuration. The reconfiguration is is started based on an initiation signal.
+--   The 'stewart' fetches the configuration from the configuration repo and loads it into the worker.
+steward :: Num a => Signal (Maybe Integer)   -- ^The initiation signal specifies the index of the function to be loaded
+                 -> Signal (a -> a)      -- ^The signal receiving the functions from the configuration repository
+                 -> (Signal Integer,         -- ^The signal that specifies the index
+                                         --  of the function to be loaded from the configuration repo
+                    Signal (a -> a))     -- ^The signal with the functions that
+                                         --  shall be loaded into the worker  
+steward = mealy22 ns o (0, (+1))
+   where -- Next state function
+         ns :: (Integer, a -> a) -> Maybe Integer -> (a -> a) -> (Integer, a -> a)
+         ns state Nothing    _   = state
+         ns _     (Just x)  conf = (x, conf)
+         -- Output function
+         o :: (Integer, a -> a) -> Maybe Integer -> (a -> a) -> (Integer, a -> a)
+         o  _     Nothing   conf = (0, conf)
+         o  _     (Just x)  conf = (x, conf)
 
 -- | The 'configRepo' (configuration repository) contains the different functions
 --   which are fetched by the 'steward' and loaded into the 'worker'.
